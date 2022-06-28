761ff092fa1187bc287996a5659585f3d64f6ae7
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-06-28 17:46:17 +0200

    added Plot Data and Bar Chart

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from turtle import position
 import tweepy
 import json
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+
 
 def authentication(creds):
 
@@ -15,70 +16,106 @@ def authentication(creds):
 
     return tweepy.API(auth)
 
+
 def read_creds(filename):
 
     with open(filename) as f:
         credentials = json.load(f)
     return credentials
 
+
 def create_csv_from_timeline():
     tweets = api.home_timeline()
-    columns=["Time", "User", "Tweet"]
+    columns = ["Time", "User", "Tweet"]
     data = []
-    
+
     for tweet in tweets:
         data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
-    
+
     df = pd.DataFrame(data, columns=columns)
     df.to_csv("tweety.csv")
+
 
 def create_csv_from_user_tweets(user):
-    tweets = api.user_timeline(screen_name=user, count=200, tweet_mode="extended")
-    columns=["Time", "User", "Tweet"]
+    tweets = api.user_timeline(
+        screen_name=user, count=200, tweet_mode="extended")
+    columns = ["Time", "User", "Tweet"]
     data = []
-    
+
     for tweet in tweets:
-        data.append([tweet.created_at, tweet.user.screen_name, tweet.full_text])
-    
+        data.append(
+            [tweet.created_at, tweet.user.screen_name, tweet.full_text])
+
     df = pd.DataFrame(data, columns=columns)
-    df.to_csv("tweety.csv")
+    return df
+    # df.to_csv("tweety.csv")
+
 
 def user_tweets(user):
-    tweets = api.user_timeline(screen_name=user, count=200, tweet_mode="extended")
+    tweets = api.user_timeline(
+        screen_name=user, count=200, tweet_mode="extended")
     for tweet in tweets:
         print(tweet.created_at, tweet.full_text)
 
-#### STREAMLIT ####
-st.set_page_config(page_title="My Website", page_icon=":bar_chart:", layout="wide")
-st.title("Streamlit with Twitter")
 
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.subheader("Save tweets to CSV file.")
-        st.write("Provide Twitter ID:")
-    with right_column:
-        st.subheader("Save tweets to DB.")
-        st.write("Provide Twitter ID:")
+@st.cache
+def convert_df(df):
+    # Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
 
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.write("plot your data") 
-        uploaded_file = st.file_uploader("Choose a CSV file.", type="csv")
-    with right_column:
-        if uploaded_file:
-            df = pd.read_csv(uploaded_file, encoding="utf-8")
-            st.dataframe(df[['User','Tweet']])
-    st.write("---")
+
+def main():
+    #### STREAMLIT ####
+    st.set_page_config(page_title="My Website",
+                       page_icon=":bar_chart:", layout="wide")
+    st.title("Streamlit with Twitter")
+
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.subheader("Save tweets to the file.")
+            user_input_csv = st.text_input(
+                "Provide Twitter ID:", key="csv")
+            
+            csv = convert_df(create_csv_from_user_tweets(user_input_csv))
+            st.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name='data.csv',
+                mime='text/csv',
+                )
+            
+        with right_column:
+            st.subheader("Save tweets to DB.")
+            st.write("Soon...")
+
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.write("Plot your data.")
+            uploaded_file = st.file_uploader("Choose a CSV file.", type="csv")
+        with right_column:
+            if uploaded_file:
+                df = pd.read_csv(uploaded_file, encoding="utf-8")
+                st.dataframe(df[['User', 'Tweet']])
+                fig = pd.DataFrame(df['User'].value_counts())
+                
+    with st.container():
+        st.write("---")
+        left_column, middle_column, right_column = st.columns(3)
+        with left_column:
+            pass
+        with middle_column:
+            if uploaded_file:
+                st.bar_chart(fig)
+        with right_column:
+            pass
         
 if __name__ == '__main__':
     credentials = 'credentials.json'
     api = authentication(credentials)
-    #create_csv_from_user_tweets("pwojtarowicz1")
-    #user_tweets("pwojtarowicz1")
-
-
-    
+    main()
+    # create_csv_from_user_tweets("pwojtarowicz1")
+    # user_tweets("pwojtarowicz1")

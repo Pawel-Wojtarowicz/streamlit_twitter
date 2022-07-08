e8f58a57588df93c5b33afcb724a2baaccfce133
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-07-08 12:56:01 +0200

    working on a new method to retrieve tweets from the range of days specified by the user

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from tkinter.tix import TCL_WINDOW_EVENTS
 import tweepy
 import json
 import pandas as pd
@@ -47,16 +46,17 @@ def create_csv_from_user_tweets(users):
     return df, error
 
 
-# def create_csv_from_user_tweets_from_the_time_interval(provided_user_list, user_date):
-#     columns = ["User", "Tweets"]
-#     tweets = []
+def create_csv_from_user_tweets_from_the_time_interval(provided_user_list, user_date):
+    columns = ["User", "Tweets"]
+    tweets = []
 
-#     for user in provided_user_list:
-#         query = f"(from:{user}) until:{user_date[0]} since:{user_date[1]}"
-#         for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-#             TCL_WINDOW_EVENTS.append([tweet.user.username, tweet.content])
-#     df = pd.DataFrame(tweets, columns=columns)
-#     return df
+    for user in provided_user_list:
+        query = f"(from:{user}) until:{user_date[1]} since:{user_date[0]}"
+        for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+            tweets.append([tweet.user.username, tweet.content])
+    df = pd.DataFrame(tweets, columns=columns)
+    return df
+
 
 @st.cache
 def convert_df(df):
@@ -70,35 +70,54 @@ def main():
     st.title("Streamlit with Twitter")
 
     with st.container():
-        st.write("---")
-        st.subheader("Save tweets to the file.")
-        keywords = st_tags(label="Enter Accounts from Twitter:",
-                           text="Press enter to add more", value=["elonmusk", "barackobama"])
-        csv = convert_df(create_csv_from_user_tweets(keywords)[0])
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.write("---")
+            st.subheader("Save tweets to the file usinng Twitter scrapper")
+            keywords = st_tags(label="Enter Accounts from Twitter:",
+                               text="Press enter to add more", value=["elonmusk", "barackobama"], key="scrapper")
+        with right_column:
+            st.write("---")
+            st.subheader("Save tweets to the file using Twitter API")
+            keywords = st_tags(label="Enter Accounts from Twitter:",
+                               text="Press enter to add more", value=["trzaskowski_", "bweglarczyk"], key="api")
+            # csv = convert_df(create_csv_from_user_tweets(keywords)[0])
 
-        if create_csv_from_user_tweets(keywords)[1]:
-            st.write("User(s):", create_csv_from_user_tweets(
-                keywords)[1], "not found.")
-        else:
-            pass
+            # if create_csv_from_user_tweets(keywords)[1]:
+            #     st.write("User(s):", create_csv_from_user_tweets(
+            #         keywords)[1], "not found.")
+            # else:
+            #     pass
+
     with st.container():
-        # today = datetime.today().date()
-        # left_column, right_column = st.columns(2)
-        # with left_column:
-        #     from_date = st.date_input("From:", today)
-        #     st.write('From:', from_date)
+        today = datetime.today().date()
+        dates = []
+        left_column, middle_column, right_column = st.columns([1, 1, 2])
+        with left_column:
+            dates.append(st.date_input("From:", today))
+        with middle_column:
+            dates.append(st.date_input("To:", today))
 
-        # with right_column:
-        #     till_date = st.date_input("To:", today)
-        #     st.write('To:', till_date)
-
+            csv = convert_df(
+                create_csv_from_user_tweets_from_the_time_interval(keywords, dates))
+            
         st.download_button(
-            label="Download data as CSV",
-            data=csv,
-            file_name='data.csv',
-            mime='text/csv',
-        )
-        pass
+                label="Download data as CSV",
+                data=csv,
+                file_name='data.csv',
+                mime='text/csv',
+                key="scrappper"
+            )
+        with right_column:
+    
+            st.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name='data.csv',
+                mime='text/csv',
+                key="api"
+            )
+
     with st.container():
         st.write("---")
         left_column, right_column = st.columns(2)
@@ -119,14 +138,6 @@ def main():
         st.write("---")
         left_column, middle_column, right_column = st.columns(3)
         with left_column:
-            # csv = convert_df(create_csv_from_user_tweets_from_the_time_interval(
-            #     users=("elonmusk", "barackobama"), dates=("2022-07-06", "2022-07-01")))
-
-            # st.download_button(
-            #     label="Download data as CSV",
-            #     data=csv,
-            #     file_name='data.csv',
-            #     mime='text/csv',)
             pass
         with middle_column:
             if uploaded_file:
